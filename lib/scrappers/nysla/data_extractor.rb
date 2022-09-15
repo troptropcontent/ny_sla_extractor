fdaaa37219cb3f94543e1ed2597562d397fa4af2
Tom Ecrepont <tomecrepont@gmail.com>
2022-09-15 21:59:54 +0200

    Add page number to the data extractor from link

--- lib/scrappers/nysla/data_extractor.rb
+++ lib/scrappers/nysla/data_extractor.rb
@@ -6,12 +6,17 @@ module Nysla
       @name = data_from_text['name']
       @localisation = data_from_text['localisation']
       @link = link_element.to_h['href']
+      @number_of_pages = data_from_href['numpages'].first.to_i
     end
 
     private
 
     def data_from_text
       @data_from_text ||= @link_element.text.match(/(?<name>.+) - (?<license>\d{7})\((?<localisation>.+)\)$/).named_captures
+    end
+
+    def data_from_href
+      CGI.parse(URI.parse(@link).query)
     end
   end
 end
