c6b4c0aaf267308a76aa3c89670a93dfc471a528
Tom Ecrepont <tomecrepont@gmail.com>
2022-09-15 22:00:14 +0200

    add the number of pages to the scrapper

--- lib/scrappers/nysla/scraper.rb
+++ lib/scrappers/nysla/scraper.rb
@@ -5,13 +5,14 @@ require_relative 'date_extractor'
 
 module Nysla
   class Scraper
-    attr_reader :links, :month, :year
+    attr_reader :links, :month, :year, :number_of_pages
 
     BASE_URL = 'https://www.nyslapricepostings.com/PricePostings.asp?postingType=WR'
     def initialize
       @links = fetched_links
       @month = fetched_date['month'].downcase
       @year = fetched_date['year']
+      @number_of_pages = fetch_total_number_of_pages
     end
 
     private
@@ -26,6 +27,14 @@ module Nysla
 
     def fetched_date
       @fetched_date ||= DateExtractor.new(html_file).call
+    end
+
+    def fetch_total_number_of_pages
+      @links.sum do |link_element|
+        url = link_element.to_h['href']
+        query_params = URI.parse(url).query
+        CGI.parse(query_params)['numpages'].first.to_i
+      end
     end
   end
 end
